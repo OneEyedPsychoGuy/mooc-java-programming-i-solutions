3f716d35740479e9cca814fc05f1621503cfc60e
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-04-28 23:47:10 -0700

    Add part02-Part02_08.NumberOfNumbers

---
+++ part02/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class NumberOfNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = -1;
+        int count = -1;
+
+        while(num != 0) {
+            System.out.println("Give a number:");
+            num = Integer.valueOf(scanner.nextLine());
+            count++;
+        }
+        System.out.println("Number of numbers: " + count);
+
+        scanner.close();
+    }
+}
