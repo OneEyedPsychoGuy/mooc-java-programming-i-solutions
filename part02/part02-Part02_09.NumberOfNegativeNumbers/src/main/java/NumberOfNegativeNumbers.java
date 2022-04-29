f78a620d53c05ec24c3b665f27190dea03f57842
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-04-29 00:06:25 -0700

    Add part02-Part02_09.NumberOfNegativeNumbers

---
+++ part02/part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class NumberOfNegativeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = -1;
+        int count = 0;
+
+        while(num != 0) {
+            System.out.println("Give a number:");
+            num = Integer.valueOf(scanner.nextLine());
+
+            if(num < 0) {
+                count++;
+            }
+        }
+        
+        System.out.println("Number of negative numbers: " + count);
+
+        scanner.close();
+    }
+}
