0df94d8c456664b0cd147199938d8ef3d9574d6c
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-04-29 21:04:31 -0700

    Add part02-Part02_13.AverageOfPositiveNumbers

---
+++ part02/part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class AverageOfPositiveNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = -1;
+        int count = 0;
+        int sum = 0;
+
+        while(num != 0) {
+            System.out.println("Give a number:");
+            num = Integer.valueOf(scanner.nextLine());
+            if(num > 0) {
+                count++;
+                sum += num;
+            }
+        }
+
+        if(count > 0) {
+            System.out.println(1.0 * sum / count);
+        } else {
+            System.out.println("Cannot calculate the average");
+        }
+
+        scanner.close();
+    }
+}
