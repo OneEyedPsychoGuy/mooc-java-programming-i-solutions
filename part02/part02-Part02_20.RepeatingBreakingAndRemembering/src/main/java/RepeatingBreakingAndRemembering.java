3ae3e6df9fdb559ca179ac85ddcf68285537d4c2
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-05-02 07:56:40 -0700

    Add part02-Part02_20.RepeatingBreakingAndRemembering Part 2: Sum of numbers

--- part02/part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
+++ part02/part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
@@ -3,14 +3,20 @@ import java.util.Scanner;
 public class RepeatingBreakingAndRemembering {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int num = 0;
+        int num = 0, sum = 0;
         
         System.out.println("Give numbers:");
-        while(num != -1) {
+        while(true) {
             num = Integer.valueOf(scanner.nextLine());
+            if(num == -1) {
+                break;
+            }
+
+            sum += num;
         }
 
         System.out.println("Thx! Bye!");
+        System.out.println("Sum: " + sum);
 
         scanner.close();
     }
