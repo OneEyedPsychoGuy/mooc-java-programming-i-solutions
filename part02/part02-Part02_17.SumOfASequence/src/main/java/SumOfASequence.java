64ad9c8652dee46d3cbc74d9263ab52394dddc55
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-05-02 06:43:18 -0700

    Update part02-Part02_17.SumOfASequence

--- part02/part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
+++ part02/part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
@@ -4,10 +4,9 @@ public class SumOfASequence {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int sum = 0;
-        int last = 0;
         
         System.out.print("Last number? ");
-        last = Integer.valueOf(scanner.nextLine());
+        int last = Integer.valueOf(scanner.nextLine());
 
         for(int i = 1; i <= last; i++) {
             sum += i;
