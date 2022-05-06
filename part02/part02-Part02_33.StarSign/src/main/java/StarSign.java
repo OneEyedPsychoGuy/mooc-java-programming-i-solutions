452dc39376c2b0e6f44bc89c81d2525030be4fc5
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-05-05 20:31:50 -0700

    Add part02-Part02_33.Starsign Part 3: Printing a rectangle

--- part02/part02-Part02_33.StarSign/src/main/java/StarSign.java
+++ part02/part02-Part02_33.StarSign/src/main/java/StarSign.java
@@ -5,6 +5,8 @@ public class StarSign {
         printStars(9);
         System.out.println("");
         printSquare(4);
+        System.out.println("");
+        printRectangle(17, 3);
     }
 
     public static void printStars(int number) {
@@ -18,6 +20,12 @@ public class StarSign {
     public static void printSquare(int size) {
         for(int i = 0; i < size; i++) {
             printStars(size);
+        }
+    }
+
+    public static void printRectangle(int width, int height) {
+        for(int i = 0; i < height; i++) {
+            printStars(width);
         }
     }
 }
