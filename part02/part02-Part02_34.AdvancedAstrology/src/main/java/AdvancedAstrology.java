0493616c6fb0d3843db65094fda7f5599598c314
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-05-06 00:49:19 -0700

    Add part02-Part02_34.AdvancedAstrology Part 3: Printing a Christmas tree

--- part02/part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ part02/part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -1,6 +1,10 @@
 public class AdvancedAstrology {
     public static void main(String[] args) {
         printTriangle(4);
+        System.out.println("");
+        christmasTree(4);
+        System.out.println("");
+        christmasTree(10);
     }
 
     public static void printStars(int number) {
@@ -22,6 +26,17 @@ public class AdvancedAstrology {
         for(int i = 1; i <= size; i++) {
             printSpaces(size - i);
             printStars(i);
+        }
+    }
+
+    public static void christmasTree(int height) {
+        for(int i = 1; i <= height; i++) {
+            printSpaces(height - i);
+            printStars((i * 2) - 1);
+        }
+        for(int i = 0; i < 2; i++) {
+            printSpaces(height - 2);
+            printStars(3);
         }
     }
 }
