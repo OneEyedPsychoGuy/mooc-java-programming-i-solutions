5165f69f37cd5b647c0e1e7e157beaea9ec5b541
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-05-05 23:08:05 -0700

    Add part02-Part02_34.AdvancedAstrology Part 1: Printing stars and spaces

---
+++ part02/part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -0,0 +1,20 @@
+public class AdvancedAstrology {
+    public static void main(String[] args) {
+
+    }
+
+    public static void printStars(int number) {
+        while(number > 0) {
+            System.out.print("*");
+            number--;
+        }
+        System.out.println("");
+    }
+
+    public static void printSpaces(int number) {
+        while(number > 0) {
+            System.out.print(" ");
+            number--;
+        }
+    }
+}
