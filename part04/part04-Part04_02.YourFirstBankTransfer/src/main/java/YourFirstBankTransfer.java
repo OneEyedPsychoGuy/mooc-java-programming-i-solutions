6266c113ae9e17ca32dbc5f57552d9fe5dc05db1
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-05-21 22:09:37 -0700

    Add part04-Part04_02.YourFirstBankTransfer

---
+++ part04/part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -0,0 +1,10 @@
+public class YourFirstBankTransfer {
+    public static void main(String[] args) {
+        Account matthew = new Account("Matthews account", 1000);
+        Account my = new Account("My account", 0);
+        matthew.withdrawal(100);
+        my.deposit(100);
+        System.out.println(matthew);
+        System.out.println(my);
+    }
+}
