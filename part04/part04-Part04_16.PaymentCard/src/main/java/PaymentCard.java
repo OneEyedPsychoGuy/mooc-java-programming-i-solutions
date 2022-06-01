179539f8c8b9c317398ac16799a1ef0b2405f7ec
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-05-31 20:57:23 -0700

    Add part04-Part04_16.PaymentCard Part 2: Making transactions

--- part04/part04-Part04_16.PaymentCard/src/main/java/PaymentCard.java
+++ part04/part04-Part04_16.PaymentCard/src/main/java/PaymentCard.java
@@ -5,6 +5,14 @@ public class PaymentCard {
         this.balance = balance;
     }
 
+    public void eatAffordably() {
+        this.balance -= 2.60;
+    }
+ 
+    public void eatHeartily() {
+        this.balance -= 4.60;
+    }
+
     public String toString() {
         return "The card has a balance of " + this.balance + " euros";
     }
