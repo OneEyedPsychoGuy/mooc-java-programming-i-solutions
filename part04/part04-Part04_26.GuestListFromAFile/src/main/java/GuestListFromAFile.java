c901446309ed6f8d9949e3a86e58d533fe6aabeb
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-06-06 23:09:17 -0700

    Add part04-Part04_26.GuestListFromAFile

---
+++ part04/part04-Part04_26.GuestListFromAFile/src/main/java/GuestListFromAFile.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GuestListFromAFile {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Name of the file:");
+        String file = scanner.nextLine();
+        
+        try(Scanner fileReader = new Scanner(Paths.get(file))) {
+            while (fileReader.hasNextLine()) {
+                list.add(fileReader.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Could not read from file!");
+        }
+        System.out.println("");
+
+        System.out.println("Enter names, an empty line quits.");
+        while (true) {
+            String name = scanner.nextLine();
+            if (name.isEmpty()) {
+                break;
+            }
+
+            if (list.contains(name)) {
+                System.out.println("The name is on the list.");
+            } else {
+                System.out.println("The name is not on the list.");
+            }
+        }
+
+        System.out.println("Thank you!");
+        scanner.close();
+    }
+}
