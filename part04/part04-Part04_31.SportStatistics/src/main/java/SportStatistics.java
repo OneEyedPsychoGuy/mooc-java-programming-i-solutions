a0fa1ea254613ca91dc8e76bd284fd28eb96538e
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-06-11 00:31:28 -0700

    Add part04-Part04_31.SportStatistics Part 2: Wins and losses

--- part04/part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ part04/part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -13,15 +13,32 @@ public class SportStatistics {
 
         try(Scanner fileReader = new Scanner(Paths.get(file))) {
             int games = 0;
+            int wins = 0;
+            int losses = 0;
 
             while(fileReader.hasNextLine()) {
                 String line = fileReader.nextLine();
+                String[] row = line.split(",");
+                    
+                String homeTeam = row[0];
+                String visitingTeam = row[1];
+                int homeTeamPoints = Integer.valueOf(row[2]);
+                int visitingTeamPoints = Integer.valueOf(row[3]);
+
                 if(line.contains(team)) {
                     games++;
+                    if(homeTeam.equals(team) && homeTeamPoints > visitingTeamPoints ||
+                    visitingTeam.equals(team) && visitingTeamPoints > homeTeamPoints) {
+                        wins++;
+                    } else {
+                        losses++;
+                    }
                 }
             }
 
             System.out.println("Games: " + games);
+            System.out.println("Wins: " + wins);
+            System.out.println("Losses: " + losses);
         } catch (IOException e) {
             System.out.println("Reading the file " + file + " failed.");
         }
