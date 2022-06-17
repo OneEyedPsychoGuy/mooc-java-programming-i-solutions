9e18aa53e25f240d34e91ea088275596865aebfe
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-06-16 22:12:48 -0700

    Add part05-Part05_06.OverloadedCounter Part 2: Alternative methods

--- part05/part05-Part05_06.OverloadedCounter/src/main/java/Counter.java
+++ part05/part05-Part05_06.OverloadedCounter/src/main/java/Counter.java
@@ -19,9 +19,17 @@ public class Counter {
         }
     }
 
+    public void increase() {
+        this.increase(1);
+    }
+
     public void decrease(int decreaseBy) {
         if(decreaseBy >= 0) {
             this.value -= decreaseBy;
         }
+    }
+
+    public void decrease() {
+        this.decrease(1);
     }
 }
