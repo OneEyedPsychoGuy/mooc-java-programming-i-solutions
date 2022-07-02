9ee74fb753148e028d8479896cd6c6434529e1f7
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-07-02 00:19:29 -0700

    Add part05-Part05_17.Money Part 3: Minus

--- part05/part05-Part05_17.Money/src/main/java/MainProgram.java
+++ part05/part05-Part05_17.Money/src/main/java/MainProgram.java
@@ -1,10 +1,18 @@
 public class MainProgram {
     public static void main(String[] args) {
         Money a = new Money(10, 0);
-        Money b = new Money(3, 0);
-        Money c = new Money(5, 0);
+        Money b = new Money(3, 50);
         
-        System.out.println(a.lessThan(b));
-        System.out.println(b.lessThan(c));
+        Money c = a.minus(b);
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+        c = c.minus(a);
+                
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
     }
 }
--- part05/part05-Part05_17.Money/src/main/java/Money.java
+++ part05/part05-Part05_17.Money/src/main/java/Money.java
@@ -3,6 +3,11 @@ public class Money {
     private final int cents;
 
     public Money(int euros, int cents) {
+        if (cents > 99) {
+            euros = euros + cents / 100;
+            cents = cents % 100;
+        }
+
         this.euros = euros;
         this.cents = cents;
     }
@@ -17,6 +22,22 @@ public class Money {
 
     public Money plus(Money addition) {
         return new Money(this.euros + addition.euros, this.cents + addition.cents);
+    }
+
+    public Money minus(Money decreaser) {
+        int euros = this.euros - decreaser.euros;
+        int cents = this.cents - decreaser.cents;
+ 
+        if(cents < 0) {
+            cents = 100 + cents;
+            euros--;
+        }
+ 
+        if(euros < 0) {
+            return new Money(0, 0);
+        }
+ 
+        return new Money(euros, cents);
     }
 
     public boolean lessThan(Money compared) {
