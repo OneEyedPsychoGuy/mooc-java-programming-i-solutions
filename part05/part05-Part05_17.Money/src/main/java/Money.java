ab1c960fe4a58c47f9078036e4fd075fcdf57b1a
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-07-01 22:43:56 -0700

    Add part05-Part05_17.Money Part 1: Plus

---
+++ part05/part05-Part05_17.Money/src/main/java/Money.java
@@ -0,0 +1,29 @@
+public class Money {
+    private final int euros;
+    private final int cents;
+
+    public Money(int euros, int cents) {
+        this.euros = euros;
+        this.cents = cents;
+    }
+
+    public int euros() {
+        return this.euros;
+    }
+
+    public int cents() {
+        return this.cents;
+    }
+
+    public Money plus(Money addition) {
+        return new Money(this.euros + addition.euros, this.cents + addition.cents);
+    }
+
+    public String toString() {
+        String zero = "";
+        if (this.cents < 10) {
+            zero = "0";
+        }
+        return this.euros + "." + zero + this.cents + "e";
+    }
+}
