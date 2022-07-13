53d456d6561498aaff29ba9e60a3c783d1d1240f
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-07-12 23:08:19 -0700

    Add part06-Part06_08.CargoHold Part 6: Hold-class

---
+++ part06/part06-Part06_08.CargoHold/src/main/java/Hold.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Hold {
+    private ArrayList<Suitcase> suitcases;
+    private int maxWeight;
+    private int currentWeight;
+
+    public Hold(int maxWeight) {
+        this.suitcases = new ArrayList<>();
+        this.maxWeight = maxWeight;
+        this.currentWeight = 0;
+    }
+
+    public void addSuitcase(Suitcase suitcase) {
+        if(this.currentWeight + suitcase.totalWeight() <= this.maxWeight) {
+            this.suitcases.add(suitcase);
+            this.currentWeight += suitcase.totalWeight();
+        }
+    }
+
+    public String toString() {
+        if(this.suitcases.isEmpty()) {
+            return "no suitcases (0 kg)";
+        }
+        return this.suitcases.size() + " item" + (this.suitcases.size() == 1 ? "" : "s") + " (" + this.currentWeight + " kg)";
+    }
+}
