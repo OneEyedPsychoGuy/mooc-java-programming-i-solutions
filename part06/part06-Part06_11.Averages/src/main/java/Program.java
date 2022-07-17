b679f3280872827ca7bee47df761e8184a11174c
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-07-16 22:09:13 -0700

    Add part06-Part06_11.Averages Part 3: Prints in the user interface

--- part06/part06-Part06_11.Averages/src/main/java/Program.java
+++ part06/part06-Part06_11.Averages/src/main/java/Program.java
@@ -1,10 +1,11 @@
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         GradeRegister register = new GradeRegister();
-        register.addGradeBasedOnPoints(93);
-        register.addGradeBasedOnPoints(91);
-        register.addGradeBasedOnPoints(92);
+        UserInterface ui = new UserInterface(register, scanner);
         
-        System.out.println(register.averageOfPoints());
+        ui.start();
     }
 }
