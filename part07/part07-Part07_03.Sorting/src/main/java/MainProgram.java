ef9bf27cdcf12e1910322b1bc7d87d2eea129f13
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-07-23 00:21:09 -0700

    Refactor part07-Part07_03.Sorting

--- part07/part07-Part07_03.Sorting/src/main/java/MainProgram.java
+++ part07/part07-Part07_03.Sorting/src/main/java/MainProgram.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class MainProgram {
     public static void main(String[] args) {
         int[] numbers = {8, 3, 7, 9, 1, 2, 4};
@@ -17,23 +15,23 @@ public class MainProgram {
     }
 
     public static int indexOfSmallest(int[] array) {
-        int index = 0;
+        int smallestIndex = 0;
         for(int i = 0; i < array.length; i++) {
-            if(array[index] > array[i]) {
-                index = i;
+            if(array[smallestIndex] > array[i]) {
+                smallestIndex = i;
             }
         }
-        return index;
+        return smallestIndex;
     }
 
-    public static int indexOfSmallestFrom(int[] table, int startIndex) {
-        int index = startIndex;
-        for(int i = startIndex; i < table.length; i++) {
-            if(table[index] > table[i]) {
-                index = i;
+    public static int indexOfSmallestFrom(int[] array, int startIndex) {
+        int smallestIndex = startIndex;
+        for(int i = startIndex; i < array.length; i++) {
+            if(array[smallestIndex] > array[i]) {
+                smallestIndex = i;
             }
         }
-        return index;
+        return smallestIndex;
     }
 
     public static void swap(int[] array, int index1, int index2) {
@@ -43,13 +41,9 @@ public class MainProgram {
     }
 
     public static void sort(int[] array) {
-        System.out.println(Arrays.toString(array)); //original
-
         for(int i = 0; i < array.length - 1; i++) {
             int smallestIndex = MainProgram.indexOfSmallestFrom(array, i);
             MainProgram.swap(array, i, smallestIndex);
-
-            System.out.println(Arrays.toString(array));
         }
     }
 }
