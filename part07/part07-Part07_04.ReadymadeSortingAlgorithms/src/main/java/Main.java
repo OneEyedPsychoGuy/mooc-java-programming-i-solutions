0b605e74a0fb1e4ec4b47037e5d2114a7c9c87b4
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-07-23 23:05:00 -0700

    Add part07-Part07_04.ReadyMadeSortingAlgorithms

---
+++ part07/part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+
+    public static void sort(int[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void sort(String[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void sortIntegers(ArrayList<Integer> list) {
+        Collections.sort(list);
+    }
+
+    public static void sortStrings(ArrayList<String> list) {
+        Collections.sort(list);
+    }
+}
