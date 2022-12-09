166accf556f93a090974f7ae2c2d92d0d7d6a399
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-08 17:30:40 -0800

    Finalize part01-Part01_05.Message

--- part01-Part01_05.Message/Message.java
+++ part01-Part01_05.Message/Message.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class Message {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Write a message:");
-        String message = scanner.nextLine();
-        System.out.println(message);
+        System.out.println(scanner.nextLine());
+
         scanner.close();
     }
 }
