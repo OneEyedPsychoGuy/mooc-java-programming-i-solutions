c687e061150a7a0f897d324e9005c47929eee966
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-08 19:21:36 -0800

    Finalize part01-Part01_31.GradesAndPoints

--- part01-Part01_31.GradesAndPoints/GradesAndPoints.java
+++ part01-Part01_31.GradesAndPoints/GradesAndPoints.java
@@ -3,26 +3,26 @@ import java.util.Scanner;
 public class GradesAndPoints {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String grade = "";
 
         System.out.println("Give points [0-100]:");
         int points = Integer.valueOf(scanner.nextLine());
-        String grade = "";
 
         if(points < 0) {
             grade = "impossible";
-        } else if(points >= 0 && points < 50) {
+        } else if(points < 50) {
             grade = "failed";
-        } else if(points >= 50 && points < 60) {
+        } else if(points < 60) {
             grade = "1";
-        } else if(points >= 60 && points < 70) {
+        } else if(points < 70) {
             grade = "2";
-        } else if(points >= 70 && points < 80) {
+        } else if(points < 80) {
             grade = "3";
-        } else if(points >= 80 && points < 90) {
+        } else if(points < 90) {
             grade = "4";
-        } else if(points >= 90 && points <= 100) {
+        } else if(points <= 100) {
             grade = "5";
-        } else if(points > 100) {
+        } else {
             grade = "incredible";
         }
 
