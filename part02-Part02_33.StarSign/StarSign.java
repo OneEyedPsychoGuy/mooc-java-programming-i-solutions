232d199f64c5117a95f79dac29ce2b38d36e5448
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-09 21:29:46 -0800

    Finalize part02-Part02_33.StarSign

---
+++ part02-Part02_33.StarSign/StarSign.java
@@ -0,0 +1,29 @@
+public class StarSign {
+    public static void main(String[] args) {}
+
+    public static void printStars(int stars) {
+        while(stars > 0) {
+            System.out.print("*");
+            stars--;
+        }
+        System.out.println();
+    }
+
+    public static void printSquare(int side) {
+        for(int i = 0; i < side; i++) {
+            printStars(side);
+        }
+    }
+
+    public static void printRectangle(int width, int height) {
+        for(int i = 0; i < height; i++) {
+            printStars(width);
+        }
+    }
+
+    public static void printTriangle(int side) {
+        for(int i = 1; i <= side; i++) {
+            printStars(i);
+        }
+    }
+}
