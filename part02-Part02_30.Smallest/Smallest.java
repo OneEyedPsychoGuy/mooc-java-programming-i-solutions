254f764d333303950efa59b85762c78795df7ef0
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-09 21:15:11 -0800

    Finalize part02-Part02_30.Smallest

--- part02-Part02_30.Smallest/Smallest.java
+++ part02-Part02_30.Smallest/Smallest.java
@@ -1,7 +1,5 @@
 public class Smallest {
-    public static void main(String[] args) {
-        System.out.println("Smallest: " + smallest(2, 7));
-    }
+    public static void main(String[] args) {}
 
     public static int smallest(int number1, int number2) {
         if(number1 < number2) {
