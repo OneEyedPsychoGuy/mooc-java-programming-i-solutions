fc849eb4d0937077968f62983a9fe866a679c6a1
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-09 21:33:45 -0800

    Finalize part02-Part02_34.AdvancedAstrology

---
+++ part02-Part02_34.AdvancedAstrology/AdvancedAstrology.java
@@ -0,0 +1,36 @@
+public class AdvancedAstrology {
+    public static void main(String[] args) {}
+
+    public static void printStars(int stars) {
+        while(stars > 0) {
+            System.out.print("*");
+            stars--;
+        }
+        System.out.println();
+    }
+
+    public static void printSpaces(int spaces) {
+        while(spaces > 0) {
+            System.out.print(" ");
+            spaces--;
+        }
+    }
+
+    public static void printTriangle(int side) {
+        for(int i = 1; i <= side; i++) {
+            printSpaces(side - i);
+            printStars(i);
+        }
+    }
+
+    public static void christmasTree(int height) {
+        for(int i = 1; i <= height; i++) {
+            printSpaces(height - i);
+            printStars((i * 2) - 1);
+        }
+        for(int i = 0; i < 2; i++) {
+            printSpaces(height - 2);
+            printStars(3);
+        }
+    }
+}
