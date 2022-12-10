1bf1702335042fe57746e86bb122a68d66c90ac6
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-09 21:05:24 -0800

    Finalize part02-Part02_27.NumberUno

--- part02-Part02_27.NumberUno/NumberUno.java
+++ part02-Part02_27.NumberUno/NumberUno.java
@@ -1,7 +1,5 @@
 public class NumberUno {
-    public static void main(String[] args) {
-
-    }
+    public static void main(String[] args) {}
 
     public static int numberUno() {
         return 1;
