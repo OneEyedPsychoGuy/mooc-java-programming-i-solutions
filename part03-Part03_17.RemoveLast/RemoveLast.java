f0d242ba17afcda25609e8dc2705f86ea43eaa89
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-11 20:54:59 -0800

    Finalize part03-Part03_17.RemoveLast

---
+++ part03-Part03_17.RemoveLast/RemoveLast.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class RemoveLast {
+    public static void main(String[] args) {}
+
+    public static void removeLast(ArrayList<String> list) {
+        if(list.size() == 0) {
+            return;
+        }
+        list.remove(list.size() - 1);
+    }
+}
