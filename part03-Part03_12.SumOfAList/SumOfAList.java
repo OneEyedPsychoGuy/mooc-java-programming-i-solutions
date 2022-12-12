29a36b5d743da5c3bd6646c20e5103824fca07af
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-11 19:55:19 -0800

    Finalize part03-Part03_12.SumOfAList

--- part03-Part03_12.SumOfAList/SumOfAList.java
+++ part03-Part03_12.SumOfAList/SumOfAList.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 public class SumOfAList {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> nums = new ArrayList<>();
 
         while (true) {
-            int input = Integer.valueOf(scanner.nextLine());
-            if (input == -1) {
+            int num = Integer.valueOf(scanner.nextLine());
+            if (num == -1) {
                 break;
             }
-            list.add(input);
+            nums.add(num);
         }
-        System.out.println("");
+        System.out.println();
 
         int sum = 0;
-        for(Integer num : list) {
+        for(Integer num : nums) {
             sum += num;
         }
 
