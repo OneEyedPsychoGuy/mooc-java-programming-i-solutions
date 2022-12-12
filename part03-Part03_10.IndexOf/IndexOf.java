0b0756ce4b21b8cec09f1749b2e75158aa537cb4
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-11 19:46:53 -0800

    Finalize part03-Part03_10.IndexOf

--- part03-Part03_10.IndexOf/IndexOf.java
+++ part03-Part03_10.IndexOf/IndexOf.java
@@ -4,22 +4,22 @@ import java.util.Scanner;
 public class IndexOf {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> nums = new ArrayList<>();
 
         while (true) {
             int num = Integer.valueOf(scanner.nextLine());
             if (num == -1) {
                 break;
             }
-            list.add(num);
+            nums.add(num);
         }
-        System.out.println("");
+        System.out.println();
 
         System.out.println("Search for? ");
         int search = Integer.valueOf(scanner.nextLine());
 
-        for(int i = 0; i < list.size(); i++) {
-            if(search == list.get(i)) {
+        for(int i = 0; i < nums.size(); i++) {
+            if(search == nums.get(i)) {
                 System.out.println(search + " is at index " + i);
             }
         }
