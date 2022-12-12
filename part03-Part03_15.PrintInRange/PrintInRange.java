0661d105ac77beff1f3806500f0ca5b13b715841
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-11 20:45:01 -0800

    Finalize part03-Part03_15.PrintInRange

---
+++ part03-Part03_15.PrintInRange/PrintInRange.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class PrintInRange {
+    public static void main(String[] args) {}
+    
+    public static void printNumbersInRange(ArrayList<Integer> nums, int lower, int upper) {
+        for(Integer num : nums) {
+            if(num >= lower && num <= upper) {
+                System.out.println(num);
+            }
+        }
+    }
+}
