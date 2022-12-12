d302f0e644b92b74a924ace67410010a2d6c4d1d
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-11 21:05:38 -0800

    Finalize part03-Part03_20.SumOfArray

--- part03-Part03_20.SumOfArray/SumOfArray.java
+++ part03-Part03_20.SumOfArray/SumOfArray.java
@@ -1,8 +1,5 @@
 public class SumOfArray {
-    public static void main(String[] args) {
-        int[] numbers = {5, 1, 3, 4, 2};
-        System.out.println(sumOfNumbersInArray(numbers));
-    }
+    public static void main(String[] args) {}
 
     public static int sumOfNumbersInArray(int[] array) {
         int sum = 0;
