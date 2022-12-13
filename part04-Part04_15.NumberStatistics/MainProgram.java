3ebc629c3a720fed274ea2fbb72e7ffc078bb6cf
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-12 21:09:26 -0800

    Finalize part04-Part04_15.NumberStatistics

--- part04-Part04_15.NumberStatistics/MainProgram.java
+++ part04-Part04_15.NumberStatistics/MainProgram.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class MainProgram {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Statistics statAll = new Statistics();
         Statistics statEven = new Statistics();
         Statistics statOdd = new Statistics();
-        Scanner scanner = new Scanner(System.in);
  
         System.out.println("Enter numbers:");
         while(true) {
