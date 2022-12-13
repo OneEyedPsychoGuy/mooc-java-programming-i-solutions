a89eea128232d480f7b741f187424719a5092058
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-12 19:39:15 -0800

    Finalize part03-Part03_32.PersonalDetails

---
+++ part03-Part03_32.PersonalDetails/PersonalDetails.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PersonalDetails {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String longest = "";
+        int sum = 0;
+        int count = 0;
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+
+            String[] data = input.split(",");            
+            if(data[0].length() > longest.length()) {
+                longest = data[0];
+            }
+            sum += Integer.valueOf(data[1]);
+            count++;
+        }
+
+        System.out.println("Longest name: " + longest);
+        System.out.println("Average of the birth years: " + (1.0 * sum / count));
+        scanner.close();
+    }
+}
