0472a235fe17210ea400e9676b579b37e28ec0de
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-12 19:45:14 -0800

    Finalize part04-Part04_01.YourFirstAccount

--- part04-Part04_01.YourFirstAccount/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/YourFirstAccount.java
@@ -1,6 +1,6 @@
 public class YourFirstAccount {
     public static void main(String[] args) {
-        Account example = new Account("Example's account", 100);
+        Account example = new Account("Example account", 100);
         example.deposit(20);
         System.out.println(example);
     }
