93d7a855eb38fc74bf35da35f6fc44a47b6ea52e
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-12 20:28:47 -0800

    Finalize part04-Part04_10.Song

--- part04-Part04_10.Song/src/main/java/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    Song garden = new Song("In The Garden", 10910);
-    System.out.println("The song " + garden.name() + " has a length of " + garden.length() + " seconds.");
-  }
-}
