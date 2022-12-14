944ab80f9ce5957b7d929c2af5b8523b82a94072
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-13 22:21:38 -0800

    Finalize part01-Part01_32.OddOrEven

--- part01-Part01_32.OddOrEven/OddOrEven.java
+++ part01-Part01_32.OddOrEven/OddOrEven.java
@@ -6,13 +6,12 @@ public class OddOrEven {
 
         System.out.println("Give a number:");
         int value = Integer.valueOf(scanner.nextLine());
+        scanner.close();
         
         if(value % 2 == 0) {
             System.out.println("Number " + value + " is even.");
         } else {
             System.out.println("Number " + value + " is odd.");
         }
-
-        scanner.close();
     }
 }
