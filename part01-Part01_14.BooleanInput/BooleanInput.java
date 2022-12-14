23003e31044b2927c9902fd143d38fc129aab3ee
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-13 20:53:59 -0800

    Finalize part01-Part01_14.BooleanInput

--- part01-Part01_14.BooleanInput/BooleanInput.java
+++ part01-Part01_14.BooleanInput/BooleanInput.java
@@ -6,8 +6,8 @@ public class BooleanInput {
 
         System.out.println("Write something:");
         boolean value = Boolean.valueOf(scanner.nextLine());
-        System.out.println("True or false? " + value);
-        
         scanner.close();
+        
+        System.out.println("True or false? " + value);
     }
 }
