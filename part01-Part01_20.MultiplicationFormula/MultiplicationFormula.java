2ae10890d46f2e0800a6bccb283e5fea71a45f94
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-13 21:13:18 -0800

    Finalize part01-Part01_20.MultiplicationFormula

--- part01-Part01_20.MultiplicationFormula/MultiplicationFormula.java
+++ part01-Part01_20.MultiplicationFormula/MultiplicationFormula.java
@@ -8,9 +8,8 @@ public class MultiplicationFormula {
         int first = Integer.valueOf(scanner.nextLine());
         System.out.println("Give the second number:");
         int second = Integer.valueOf(scanner.nextLine());
+        scanner.close();
 
         System.out.println(first + " * " + second + " = " + (first * second));
-
-        scanner.close();
     }
 }
