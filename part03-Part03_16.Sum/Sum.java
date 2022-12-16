ae7632a5a59c096e7ab7a7e9cadd78bcf399bada
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-16 15:37:24 -0800

    Finalize part03-Part03_16.Sum

--- part03-Part03_16.Sum/Sum.java
+++ part03-Part03_16.Sum/Sum.java
@@ -1,8 +1,6 @@
 import java.util.ArrayList;
 
 public class Sum {
-    public static void main(String[] args) {}
-
     public static int sum(ArrayList<Integer> nums) {
         int sum = 0;
         for(Integer num : nums) {
