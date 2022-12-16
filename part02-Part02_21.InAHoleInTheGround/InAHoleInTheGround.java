8e9fe1a2691e739a13b972136f677a878a5e0e65
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-16 06:33:13 -0800

    Finalize part02-Part02_21.InAHoleInTheGround

--- part02-Part02_21.InAHoleInTheGround/InAHoleInTheGround.java
+++ part02-Part02_21.InAHoleInTheGround/InAHoleInTheGround.java
@@ -1,8 +1,4 @@
 public class InAHoleInTheGround {
-    public static void main(String[] args) {
-        printText();
-    }
-    
     public static void printText() {
         System.out.println("In a hole in the ground there lived a method");
     }
