b4295deedf44eaf430fa02abd6ea9b0d75cec46a
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-16 06:38:15 -0800

    Finalize part02-Part02_24.FromParameterToOne

--- part02-Part02_24.FromParameterToOne/FromParameterToOne.java
+++ part02-Part02_24.FromParameterToOne/FromParameterToOne.java
@@ -1,6 +1,4 @@
 public class FromParameterToOne {
-    public static void main(String[] args) {}
-
     public static void printFromNumberToOne(int num) {
         for(int i = num; i > 0; i--) {
             System.out.println(i);
