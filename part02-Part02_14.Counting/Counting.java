d0cacf493472ecf8fe67302b92f33cfe18044a6b
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-16 06:20:51 -0800

    Finalize part02-Part02_14.Counting

--- part02-Part02_14.Counting/Counting.java
+++ part02-Part02_14.Counting/Counting.java
@@ -4,11 +4,10 @@ public class Counting {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num = Integer.valueOf(scanner.nextLine());
+        scanner.close();
 
         for(int i = 0; i <= num; i++) {
             System.out.println(i);
         }
-
-        scanner.close();
     }
 }
