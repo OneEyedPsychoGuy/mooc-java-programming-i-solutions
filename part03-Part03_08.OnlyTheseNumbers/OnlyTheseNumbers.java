0a2e467483a1f12eafa891d6042bcd15e78188b0
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-16 14:41:51 -0800

    Finalize part03-Part03_08.OnlyTheseNumbers

--- part03-Part03_08.OnlyTheseNumbers/OnlyTheseNumbers.java
+++ part03-Part03_08.OnlyTheseNumbers/OnlyTheseNumbers.java
@@ -8,7 +8,7 @@ public class OnlyTheseNumbers {
 
         while (true) {
             int num = Integer.valueOf(scanner.nextLine());
-            if (num == -1) {
+            if(num == -1) {
                 break;
             }
             nums.add(num);
@@ -18,11 +18,10 @@ public class OnlyTheseNumbers {
         int from = Integer.valueOf(scanner.nextLine());
         System.out.println("To where? ");
         int to = Integer.valueOf(scanner.nextLine());
+        scanner.close();
 
         for(int i = from; i <= to; i++) {
             System.out.println(nums.get(i));
         }
-        
-        scanner.close();
     }
 }
