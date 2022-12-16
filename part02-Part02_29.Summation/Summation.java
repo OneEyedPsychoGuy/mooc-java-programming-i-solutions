b5281bd6715349bd9c686abf13a3923ae59341fd
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-16 06:44:50 -0800

    Finalize part02-Part02_29.Summation

--- part02-Part02_29.Summation/Summation.java
+++ part02-Part02_29.Summation/Summation.java
@@ -1,6 +1,4 @@
 public class Summation {
-    public static void main(String[] args) {}
-
     public static int sum(int num1, int num2, int num3, int num4) {
         return num1 + num2 + num3 + num4;
     }
