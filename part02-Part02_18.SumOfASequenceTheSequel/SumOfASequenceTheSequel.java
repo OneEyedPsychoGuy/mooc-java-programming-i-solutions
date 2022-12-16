0e0db288be98623505cc61b5610d7dba9464e2f1
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-16 06:26:31 -0800

    Finalize part02-Part02_18.SumOfASequenceTheSequel

--- part02-Part02_18.SumOfASequenceTheSequel/SumOfASequenceTheSequel.java
+++ part02-Part02_18.SumOfASequenceTheSequel/SumOfASequenceTheSequel.java
@@ -9,12 +9,11 @@ public class SumOfASequenceTheSequel {
         int first = Integer.valueOf(scanner.nextLine());
         System.out.println("Last number? ");
         int last = Integer.valueOf(scanner.nextLine());
+        scanner.close();
 
         for(int i = first; i <= last; i++) {
             sum += i;
         }
         System.out.println("The sum is: " + sum);
-
-        scanner.close();
     }
 }
