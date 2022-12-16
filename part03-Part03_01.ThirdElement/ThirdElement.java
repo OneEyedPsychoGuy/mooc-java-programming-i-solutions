32a124dcc1cc7fa4e8f03f7c08618e0976c155aa
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-16 14:57:23 -0800

    Finalize part03-Part03_01.ThirdElement

--- part03-Part03_01.ThirdElement/ThirdElement.java
+++ part03-Part03_01.ThirdElement/ThirdElement.java
@@ -8,7 +8,7 @@ public class ThirdElement {
 
         while(true) {
             String input = scanner.nextLine();
-            if (input.equals("")) {
+            if(input.equals("")) {
                 break;
             }
             list.add(input);
