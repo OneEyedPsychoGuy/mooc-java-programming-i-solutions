b7a50dd746ac7fb7e756f6455c42fda6b3012944
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-16 06:40:43 -0800

    Finalize part02-Part02_26.DivisibleByThree

--- part02-Part02_26.DivisibleByThree/DivisibleByThree.java
+++ part02-Part02_26.DivisibleByThree/DivisibleByThree.java
@@ -1,6 +1,4 @@
 public class DivisibleByThree {
-    public static void main(String[] args) {}
-
     public static void divisibleByThreeInRange(int start, int end) {
         for(int i = start; i <= end; i++) {
             if(i % 3 == 0) {
