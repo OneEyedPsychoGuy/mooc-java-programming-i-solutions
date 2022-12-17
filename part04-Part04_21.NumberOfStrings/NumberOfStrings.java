6c4d799ea41d73944c11b864e2790f401eba0cef
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-17 13:26:16 -0800

    Finalize part04-Part04_21.NumberOfStrings

--- part04-Part04_21.NumberOfStrings/NumberOfStrings.java
+++ part04-Part04_21.NumberOfStrings/NumberOfStrings.java
@@ -12,8 +12,8 @@ public class NumberOfStrings {
             }
             count++;
         }
+        scanner.close();
         
         System.out.println(count);
-        scanner.close();
     }
 }
