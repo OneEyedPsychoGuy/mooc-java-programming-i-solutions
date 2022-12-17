76eca5059e4bf5d63d252c06928d10e34848b54b
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-17 14:17:40 -0800

    Finalize part04-Part04_26.GuestListFromAFile

--- part04-Part04_26.GuestListFromAFile/GuestListFromAFile.java
+++ part04-Part04_26.GuestListFromAFile/GuestListFromAFile.java
@@ -6,27 +6,27 @@ import java.util.Scanner;
 public class GuestListFromAFile {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<String> names = new ArrayList<>();
 
         System.out.println("Name of the file:");
         String file = keyboard.nextLine();
         
         try(Scanner fileReader = new Scanner(Paths.get(file))) {
             while(fileReader.hasNextLine()) {
-                list.add(fileReader.nextLine());
+                names.add(fileReader.nextLine());
             }
         } catch(IOException e) {
-            System.out.println("Could not read from file!");
+            System.out.println("Error: " + e.getMessage());
         }
 
         System.out.println("\nEnter names, an empty line quits.");
         while(true) {
             String name = keyboard.nextLine();
-            if (name.isEmpty()) {
+            if(name.isEmpty()) {
                 break;
             }
 
-            if(list.contains(name)) {
+            if(names.contains(name)) {
                 System.out.println("The name is on the list.");
             } else {
                 System.out.println("The name is not on the list.");
