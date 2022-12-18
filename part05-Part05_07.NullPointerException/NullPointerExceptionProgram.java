e970857608affd706c1e231f941dc342a91344f0
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-18 09:38:27 -0800

    Finalize part05-Part05_07.NullPointerException

--- part05-Part05_07.NullPointerException/NullPointerExceptionProgram.java
+++ part05-Part05_07.NullPointerException/NullPointerExceptionProgram.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class NullPointerExceptionProgram {
     public static void main(String[] args) {
         ArrayList<String> lines = null; // This causes a NullPointerException
+
         lines.add("Never has a man influenced physics so profoundly as Niels Bohr in the early 1900's");
         lines.add("Going back to this time period, little was known about atomic structure; Bohr set out");
         lines.add("to end the obscurity of physics. However, things didn't come easy for Bohr. He had to");
@@ -10,10 +11,8 @@ public class NullPointerExceptionProgram {
         lines.add("you and I have even heard of the quantum theory and atomic structures. Bohr came");
         lines.add("up with his quantum theory while studying...");
 
-        int index = 0;
-        while (index < lines.size()) {
-            System.out.println(lines.get(index));
-            index++;
+        for(String line : lines) {
+            System.out.println(line);
         }
     }
 }
