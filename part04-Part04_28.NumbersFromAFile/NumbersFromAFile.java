6cd9fb373df97a245fca8e9b8bdb6162f4cb675b
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-17 17:38:07 -0800

    Finalize part04-Part04_28.NumbersFromAFile

--- part04-Part04_28.NumbersFromAFile/NumbersFromAFile.java
+++ part04-Part04_28.NumbersFromAFile/NumbersFromAFile.java
@@ -4,30 +4,29 @@ import java.util.Scanner;
 
 public class NumbersFromAFile {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner keyboard = new Scanner(System.in);
 
         System.out.print("File? ");
-        String file = scanner.nextLine();
+        String file = keyboard.nextLine();
         System.out.print("Lower bound? ");
-        int lowerBound = Integer.valueOf(scanner.nextLine());
+        int lower = Integer.valueOf(keyboard.nextLine());
         System.out.print("Upper bound? ");
-        int upperBound = Integer.valueOf(scanner.nextLine());
+        int upper = Integer.valueOf(keyboard.nextLine());
+        keyboard.close();
 
         try(Scanner fileReader = new Scanner(Paths.get(file))) {
             int count = 0;
             
             while(fileReader.hasNextLine()) {
-                int number = Integer.valueOf(fileReader.nextLine());
-                if(number <= upperBound && number >= lowerBound) {
+                int num = Integer.valueOf(fileReader.nextLine());
+                if(num <= upper && num >= lower) {
                     count++;
                 }
             }
 
             System.out.println("Numbers: " + count);
-        } catch (IOException e) {
+        } catch(IOException e) {
             System.out.println("Reading the file " + file + " failed.");
         }
-
-        scanner.close();
     }
 }
