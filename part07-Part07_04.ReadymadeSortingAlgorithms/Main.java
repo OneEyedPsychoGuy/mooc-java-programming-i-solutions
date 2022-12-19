87fef0ffd2b267b132f2625e1ef85955fa855814
OneEyedPsychoGuy <promotional@jordanmeredith.com>
2022-12-19 10:46:18 -0800

    Finalize part07-Part07_04.ReadymadeSortingAlgorithms

--- part07-Part07_04.ReadymadeSortingAlgorithms/Main.java
+++ part07-Part07_04.ReadymadeSortingAlgorithms/Main.java
@@ -3,10 +3,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Main {
-    public static void main(String[] args) {
-        
-    }
-
     public static void sort(int[] array) {
         Arrays.sort(array);
     }
